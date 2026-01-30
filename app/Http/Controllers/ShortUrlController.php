c4c7ce9ce0a322b4ac6f05992eeefa9b29adf3a4
CedricPages-CNinAlps <cedric.pages@le-campus-numerique.fr>
2026-01-30 12:12:16 +0100

    Création des routes

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\ShortUrl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Psy\Util\Str;
 use Random\RandomException;
 
 /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\ShortUrl;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/shorturls',[ShortUrl::class, 'index'])->name('shorturls.index');
+    Route::get('/shorturls/create',[ShortUrl::class, 'create'])->name('shorturls.create');
+    Route::post('/shorturls/store',[ShortUrl::class, 'store'])->name('shorturls.store');
+    Route::get('/shorturls/{shorturl}/edit',[ShortUrl::class, 'edit'])->name('shorturls.edit');
+    Route::put('/shorturls/{shorturl}',[ShortUrl::class, 'update'])->name('shorturls.update');
+    Route::delete('/shorturls/{shorturl}',[ShortUrl::class, 'destroy'])->name('shorturls.destroy');
 });
 
 require __DIR__.'/auth.php';
